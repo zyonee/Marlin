2c66d78e03b374bf511ba832110743ae6ec68a08
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2019-05-06 16:55:05 +0200

    PLA bed preheat temp to 60C

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1265,7 +1265,7 @@
 
 // Preheat Constants
 #define PREHEAT_1_TEMP_HOTEND 185
-#define PREHEAT_1_TEMP_BED     45
+#define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
 #define PREHEAT_2_TEMP_HOTEND 240
