058ed814f2b5d160568ef74afff6e07f528570b7
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2019-05-17 21:13:44 +0200

    Preheat PLA: No part cooling fan on preheat

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1266,7 +1266,7 @@
 // Preheat Constants
 #define PREHEAT_1_TEMP_HOTEND 185
 #define PREHEAT_1_TEMP_BED     60
-#define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
+#define PREHEAT_1_FAN_SPEED   0 // Value from 0 to 255
 
 #define PREHEAT_2_TEMP_HOTEND 240
 #define PREHEAT_2_TEMP_BED      0
