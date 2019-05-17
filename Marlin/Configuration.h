271072cff47a8427f8c46bd3f4cfb2d79c0edb13
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2019-05-17 15:41:53 +0200

    Change to SKR v1.3 board

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_MELZI_CREALITY
+  #define MOTHERBOARD BOARD_BIGTREE_SKR_V1_3
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
