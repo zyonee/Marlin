8acc4aeffee13fa37fd1b7d4362ae492c030b0b9
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2019-06-05 23:48:19 +0200

    Change ABS preset to PETG and adjust stepper currents even more

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1469,9 +1469,9 @@
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED   0 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED      0
+#define PREHEAT_2_LABEL       "PETG"
+#define PREHEAT_2_TEMP_HOTEND 245
+#define PREHEAT_2_TEMP_BED     75
 #define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
 
 /**
