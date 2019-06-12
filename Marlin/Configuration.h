d834f858daa98344d35e00ebceffecce12399f27
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2019-06-13 00:22:13 +0200

    Update PID for Micro Swiss hotend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -457,9 +457,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 25.46
+  #define DEFAULT_Ki 2.37
+  #define DEFAULT_Kd 68.31
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
