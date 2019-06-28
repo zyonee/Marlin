48c8273e71504a094b5e42bec581a15838daf2f0
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2019-06-28 13:10:10 +0200

    Move probe Y offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -871,7 +871,7 @@
  *   (0,0)
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -47  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER -1  // Y offset: -front +behind [the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER -4  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
