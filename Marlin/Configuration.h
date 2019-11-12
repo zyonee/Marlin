f1c960530424dcaa83b064b4efec74ea5fe889c6
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2019-11-12 18:43:06 +0100

    Change PLA preheat temperature to 205C

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1440,7 +1440,7 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 190
+#define PREHEAT_1_TEMP_HOTEND 205
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED   0 // Value from 0 to 255
 
