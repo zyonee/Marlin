49e94c2a8fdde733302b47634b89587e0ea80aac
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2019-11-25 09:14:08 +0100

    Head bed PID: Own values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -529,11 +529,16 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
+  // Personal auto-tuned values
+  #define DEFAULT_bedKp 127.82
+  #define DEFAULT_bedKi 25.54
+  #define DEFAULT_bedKd 426.49
+
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  //#define DEFAULT_bedKp 10.00
+  //#define DEFAULT_bedKi .023
+  //#define DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
