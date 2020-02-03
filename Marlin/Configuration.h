21db2bef19f527424fa89f1c6c2cee9095aa8a26
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2020-02-03 21:06:54 +0100

    Switch to REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER for TFT35 compatibility

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1913,7 +1913,7 @@
 // RepRapDiscount FULL GRAPHIC Smart Controller
 // http://reprap.org/wiki/RepRapDiscount_Full_Graphic_Smart_Controller
 //
-//#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
+#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
 
 //
 // ReprapWorld Graphical LCD
@@ -1993,7 +1993,7 @@
 // This is RAMPS-compatible using a single 10-pin connector.
 // (For CR-10 owners who want to replace the Melzi Creality board but retain the display)
 //
-#define CR10_STOCKDISPLAY
+//#define CR10_STOCKDISPLAY
 
 //
 // Ender-2 OEM display, a variant of the MKS_MINI_12864
