00da0018b8323c2d7675c1747fcff82a4066e3c2
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2020-05-27 09:40:39 +0200

    Config: Increase stepper currents

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2146,7 +2146,7 @@
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT     580  // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT     600  // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS   16  // 0..256
     #define X_RSENSE     0.11
@@ -2162,7 +2162,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT     580
+    #define Y_CURRENT     600
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS   16
     #define Y_RSENSE     0.11
@@ -2178,7 +2178,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT     580
+    #define Z_CURRENT     600
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS   16
     #define Z_RSENSE     0.11
@@ -2210,7 +2210,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      567
+    #define E0_CURRENT      710
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
