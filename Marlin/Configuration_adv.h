546bafe425c54b20c88c630ab36bcf48ed17883a
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2020-06-01 22:59:29 +0200

    Set homing current for XYZ to 580

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2149,7 +2149,7 @@
 
   #if AXIS_IS_TMC(X)
     #define X_CURRENT     600  // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
+    #define X_CURRENT_HOME  580  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS   16  // 0..256
     #define X_RSENSE     0.11
     #define X_CHAIN_POS    -1  // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
@@ -2165,7 +2165,7 @@
 
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT     600
-    #define Y_CURRENT_HOME  Y_CURRENT
+    #define Y_CURRENT_HOME  580
     #define Y_MICROSTEPS   16
     #define Y_RSENSE     0.11
     #define Y_CHAIN_POS    -1
@@ -2181,7 +2181,7 @@
 
   #if AXIS_IS_TMC(Z)
     #define Z_CURRENT     600
-    #define Z_CURRENT_HOME  Z_CURRENT
+    #define Z_CURRENT_HOME  580
     #define Z_MICROSTEPS   16
     #define Z_RSENSE     0.11
     #define Z_CHAIN_POS    -1
