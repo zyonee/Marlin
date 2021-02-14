1b15709a3027254f244c9d82533917b1bce1c8bc
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2021-02-14 15:42:06 +0100

    Fix LCD offsets

--- Marlin/_Statusscreen.h
+++ Marlin/_Statusscreen.h
@@ -54,8 +54,8 @@ const unsigned char status_logo_bmp[] PROGMEM = {
 #define STATUS_HEATERS_XSPACE   20
 #if HOTENDS < 2
   #define STATUS_HEATERS_X      48
-  #define STATUS_BED_X          60
+  #define STATUS_BED_X          72
 #else
   #define STATUS_HEATERS_X      40
-  #define STATUS_BED_X          81
+  #define STATUS_BED_X          80
 #endif
