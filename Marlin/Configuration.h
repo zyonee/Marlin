e25ea4a9340133201ab5b8a91ad9ded170d448b0
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2021-02-17 09:32:48 +0100

    Config: Adjust bed size ... again.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1163,7 +1163,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 232
-#define Y_BED_SIZE 208
+#define Y_BED_SIZE 221
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
