a69608f55150a8b2523773ca36fcdfdaa118301d
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2021-02-17 10:07:06 +0100

    Config: Z steps/mm adjustment

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -742,7 +742,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 932 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 791.30, 932 }
 
 /**
  * Default Max Feed Rate (mm/s)
